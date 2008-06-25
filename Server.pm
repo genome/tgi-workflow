6fdc07f164598de22ceac20f66be7e3e97f9dcca
eclark <eclark@genome.wustl.edu>
2008-06-25 06:12:49 +0000

    status_message call was using old ->dataset property on operation instances, fixed
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@35883 127c0c0e-971a-0410-ad48-868c1a942d8d

--- Server.pm
+++ Server.pm
@@ -208,7 +208,7 @@ sub try_to_execute {
         my ($opdata,$edited_input) = @$exec_args;
 
         my $op = $opdata->operation;
-        $op->status_message('exec/' . $opdata->dataset->id . '/' . $op->name);
+        $op->status_message('exec/' . $opdata->model_instance->id . '/' . $op->name);
         $k->yield(
             'send_operation', $op, $opdata, $edited_input, sub { $opdata->do_completion; }
         );
