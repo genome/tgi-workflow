101d31778a2b6e7137af88f131d2d629fbb2122c
eclark <eclark@genome.wustl.edu>
2008-10-03 15:59:17 +0000

    Simpler api for running workflows synchronously
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@39397 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Simple.pm
@@ -0,0 +1,150 @@
+
+package Workflow::Simple;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw/run_workflow run_workflow_lsf/;
+our @EXPORT_OK = qw//;
+
+use Workflow ();
+
+use POE;
+
+sub run_workflow {
+    my $xml = shift;
+    my %inputs = @_;
+
+    my $instance;
+
+    my $w = Workflow::Model->create_from_xml($xml);
+    $w->execute(
+        input => \%inputs,
+        output_cb => sub {
+            $instance = shift;
+        },
+        error_cb => sub {
+            die 'workflow crashed during execution';
+        }
+    );
+ 
+    $w->wait;
+
+    unless ($instance) {
+        die 'workflow did not run to completion';
+    }
+
+    return $instance->output;
+}
+
+use Workflow::Server::UR;
+use Workflow::Server::Hub;
+
+sub run_workflow_lsf {
+    my $xml = shift;
+    my %inputs = @_;
+
+    die 'run_workflow_lsf is incomplete';
+
+    my $done_instance;
+    my $error;
+
+#    POE::Kernel->stop();
+#    my $pid = fork;
+#    if ($pid) {
+        ## parent process
+
+        my $after_connect = sub {
+        POE::Session->create(
+            inline_states => {
+                _start => sub {
+                    my ($kernel,$session) = @_[KERNEL,SESSION];
+
+                    $kernel->alias_set("controller");
+                    $kernel->post('IKC','publish','controller',
+                        [qw(got_plan_id got_instance_id complete error)]
+                    );
+
+                    $kernel->delay('startup',5);
+                    print "start controller " . $session->ID . "\n";
+                },
+                _stop => sub {
+                    print "stopped controller\n";
+                },
+                startup => sub {
+                    my ($kernel) = @_[KERNEL];
+
+                    $kernel->post(
+                        'IKC','call','poe://UR/workflow/load',
+                        [$xml], 'poe:got_plan_id'
+                    );
+                },
+                got_plan_id => sub {
+                    my ($kernel, $id) = @_[KERNEL,ARG0];
+                    print "Plan: $id\n";
+                    
+                    my $kernel_name = $kernel->ID;
+
+                    $_[KERNEL]->post(
+                        'IKC','call',
+                        'poe://UR/workflow/execute',
+                        [
+                            $id,
+                            \%inputs,
+                            "poe://$kernel_name/controller/complete",
+                            "poe://$kernel_name/controller/error"
+                        ],
+                        'poe:got_instance_id'
+                    );
+                },
+                got_instance_id => sub {
+                    my ($kernel, $id) = @_[KERNEL, ARG0];
+                    print "Instance: $id\n";
+                },
+                complete => sub {
+                    my ($kernel, $arg) = @_[KERNEL, ARG0];
+                    my ($id, $instance, $execution) = @$arg;
+
+                    $done_instance = $instance;
+
+                    print "Complete: $id\n";
+                    $kernel->post('IKC','post','poe://UR/workflow/quit');
+                    $kernel->alias_remove('controller');
+                },
+                error => sub {
+                    my ($kernel, $arg) = @_[KERNEL, ARG0];
+                    my ($id, $instance, $execution) = @$arg;
+
+                    print "Error: $id\n";
+
+                    $error = 1;
+                    
+                    $kernel->post('IKC','post','poe://UR/workflow/quit');
+                }
+            }
+        );
+        };
+
+        Workflow::Server::UR->start($after_connect);
+
+print "kstop\n";
+#    } elsif (defined $pid) {
+        ## child process
+
+#        Workflow::Server::Hub->start;
+#        exit;
+#    } else {
+#        die "couldnt fork";
+#    }
+
+    if (defined $error) {
+        die 'workflow crashed during execution';
+    }
+
+    unless (defined $done_instance) {
+        die 'workflow did not run to completion';
+    }
+
+    return $done_instance->output;
+}
+
+1;
---
+++ t/examples/simple.pl
@@ -0,0 +1,50 @@
+#!/gsc/bin/perl
+
+use strict;
+use warnings;
+use above 'Workflow';
+
+use Workflow::Simple;
+
+my $output = run_workflow(
+    \*DATA,
+    'model input string' => 'foo bar baz',
+    'sleep time' => 2
+);
+
+
+print Data::Dumper->new([$output])->Dump;
+
+__DATA__
+<?xml version='1.0' standalone='yes'?>
+<workflow name="Example Workflow" executor="Workflow::Executor::SerialDeferred">
+  <link fromOperation="input connector" fromProperty="sleep time" toOperation="sleep" toProperty="seconds" />
+  <link fromOperation="echo" fromProperty="result" toOperation="wait for sleep and echo" toProperty="echo result" />
+  <link fromOperation="wait for sleep and echo" fromProperty="echo result" toOperation="output connector" toProperty="result" />
+  <link fromOperation="echo" fromProperty="output" toOperation="output connector" toProperty="model output string" />
+  <link fromOperation="sleep" fromProperty="result" toOperation="wait for sleep and echo" toProperty="sleep result" />
+  <link fromOperation="input connector" fromProperty="model input string" toOperation="echo" toProperty="input" />
+  <link fromOperation="time" fromProperty="today" toOperation="output connector" toProperty="today" />
+  <operation name="wait for sleep and echo">
+    <operationtype typeClass="Workflow::OperationType::Block">
+      <property>echo result</property>
+      <property>sleep result</property>
+    </operationtype>
+  </operation>
+  <operation name="sleep">
+    <operationtype commandClass="Workflow::Test::Command::Sleep" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operation name="echo">
+    <operationtype commandClass="Workflow::Test::Command::Echo" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operation name="time">
+    <operationtype commandClass="Workflow::Test::Command::Time" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operationtype typeClass="Workflow::OperationType::Model">
+    <inputproperty>model input string</inputproperty>
+    <inputproperty>sleep time</inputproperty>
+    <outputproperty>model output string</outputproperty>
+    <outputproperty>result</outputproperty>
+    <outputproperty>today</outputproperty>
+  </operationtype>
+</workflow>
