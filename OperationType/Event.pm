9d4969a8907812316cadc9c1f4ffd04ed5c36b2e
eclark <eclark@genome.wustl.edu>
2009-03-24 17:40:11 +0000

    Partially complete workflow run-jobs implementation for jason
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@44960 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ OperationType/Event.pm
@@ -0,0 +1,146 @@
+
+package Workflow::OperationType::Event;
+
+use strict;
+use warnings;
+
+class Workflow::OperationType::Event {
+    isa => 'Workflow::OperationType',
+    is_transactional => 0,
+    id_by => [ 'event_id' ],
+    has => [
+        event_id => { is => 'String' },
+        command_class_name => { is => 'String', value => 'Genome::Model::Event' },
+        lsf_resource => { is => 'String', is_optional=>1 },
+        lsf_queue => { is => 'String', is_optional=>1 },
+    ],
+};
+
+sub get {
+    my $class = shift;
+    return $class->SUPER::get( @_ ) || $class->create ( @_ );
+}
+
+sub create {
+    my $class = shift;
+    my $params = $class->preprocess_params(@_);
+    
+    die 'missing command class' unless $params->{event_id};
+
+    my $self = $class->SUPER::create(@_);
+
+    $self->input_properties(['prior_result']);
+    $self->output_properties(['result']);    
+    
+    return $self;
+}
+
+sub create_from_xml_simple_structure {
+    my ($class, $struct) = @_;
+
+    my $id = delete $struct->{eventId};
+
+    return $class->get($id);
+}
+
+sub as_xml_simple_structure {
+    my $self = shift;
+
+    my $struct = $self->SUPER::as_xml_simple_structure;
+    $struct->{eventId} = $self->event_id;
+
+    # command classes have theirs defined in source code
+    delete $struct->{inputproperty};
+    delete $struct->{outputproperty};
+
+    return $struct;
+}
+
+# delegate to wrapped command class
+sub execute {
+    my $self = shift;
+    my %properties = @_;
+
+    umask 0022;
+
+$DB::single = $DB::stopper;
+    # Give the add-reads top level step a chance to sync database so these events
+    # show up
+    my $try_count = 10;
+    my $event;
+    while($try_count--) {
+        $event = Genome::Model::Event->load(id => $self->event_id);
+        last if ($event);
+        sleep 5;
+    }
+    unless ($event) {
+        $self->error_message('No event found with id '.$self->event_id);
+        return;
+    }
+    if (($event->event_status and $event->event_status !~ /Scheduled|Waiting/) and ! $self->reschedule) {
+        $self->error_message("Refusing to re-run event with status ".$event->event_status);
+        return;
+    }
+
+    unless ($event->verify_prior_event) {
+        my $prior_event =  $event->prior_event;
+        $self->error_message('Prior event did not verify: '. $prior_event->genome_model_event_id .' '.
+        $prior_event->event_status);
+        $event->date_completed(undef);
+        $event->event_status('Failed');
+        $event->user_name($ENV{'USER'});
+        return;
+    }
+
+#    unless ($event->model_id == $self->model_id) {
+#        $self->error_message("The model id for the loaded event ".$event->model_id.
+#                             " does not match the command line ".$self->model_id);
+#        return;
+#    }
+    
+    my $command_obj = $event;
+    $command_obj->revert;
+
+    unless ($command_obj->lsf_job_id) {
+        $command_obj->lsf_job_id($ENV{'LSB_JOBID'});
+    }
+    $command_obj->date_scheduled(UR::Time->now());
+    $command_obj->date_completed(undef);
+    $command_obj->event_status('Running');
+    $command_obj->user_name($ENV{'USER'});
+
+    UR::Context->commit();
+
+#    if ($Workflow::DEBUG_GLOBAL) {
+#        if (UNIVERSAL::can('Devel::ptkdb','brkonsub')) {
+#            Devel::ptkdb::brkonsub($command_name . '::execute');
+#        } elsif (UNIVERSAL::can('DB','cmd_b_sub')) {
+#            DB::cmd_b_sub($command_name . '::execute');
+#        } else {
+#            $DB::single=2;
+#        }
+#    }
+
+    my $rv;
+    eval { $rv = $command_obj->execute(); };
+
+    $command_obj->date_completed(UR::Time->now());
+    if ($@) {
+        $self->error_message($@);
+        $command_obj->event_status('Crashed');
+    } elsif($rv <= 1) {
+        $command_obj->event_status($rv ? 'Succeeded' : 'Failed');
+    }elsif($rv == 2) {
+        $command_obj->event_status('Waiting');
+    }
+    else {
+        $self->status_message("Unhandled positive return code: $rv...setting Succeeded");
+        $command_obj->event_status('Succeeded');
+    }
+
+    UR::Context->commit();
+
+    return { result => $rv };
+}
+
+1;
