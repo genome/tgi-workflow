4b2869a21a45f40b8b223926e952dcf9876c4be7
gsanders <gsanders@genome.wustl.edu>
2009-06-11 19:18:30 +0000

    Workflow and other tools for the Somatic Pipeline. Necessary changes made to existing tools for workflow inputs and outputs. ucsc annotator now warns and skips for irregular chromosomes. Tiering tool has corrections for parsing annotation output. Annotation output loses the variation type column as it was no longer matching the header (my bad).
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@47838 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Command/SomaticPipeline.pm
@@ -0,0 +1,65 @@
+use strict;
+use warnings;
+
+package Workflow::Command::SomaticPipeline;
+
+class Workflow::Command::SomaticPipeline {
+    is => ['Workflow::Operation::Command'],
+    workflow => sub { Workflow::Operation->create_from_xml(\*DATA); }
+};
+
+sub help_synopsis{
+    my $self = shift;
+    return "TBD";
+}
+
+# <link fromOperation="input connector" fromProperty="ucsc_output" toOperation="Annotate UCSC" toProperty="output_file" />
+1;
+__DATA__
+<?xml version='1.0' standalone='yes'?>
+
+<workflow name="Somatic Pipeline">
+
+  <link fromOperation="input connector" fromProperty="normal_file" toOperation="Somatic Sniper" toProperty="normal_file" />
+  <link fromOperation="input connector" fromProperty="tumor_file" toOperation="Somatic Sniper" toProperty="tumor_file" />
+  <link fromOperation="input connector" fromProperty="sniper_output" toOperation="Somatic Sniper" toProperty="output_file" />
+  <link fromOperation="input connector" fromProperty="adaptor_output" toOperation="Sniper Adaptor" toProperty="output_file" />
+  <link fromOperation="input connector" fromProperty="annotate_output" toOperation="Annotate Transcript Variants" toProperty="output_file" />
+  <link fromOperation="input connector" fromProperty="tier_output" toOperation="Tier Variants" toProperty="output_file" />
+  
+  <link fromOperation="input connector" fromProperty="ucsc_file" toOperation="Tier Variants" toProperty="ucsc_file" />
+
+  <link fromOperation="Somatic Sniper" fromProperty="output_file" toOperation="Sniper Adaptor" toProperty="somatic_file" />
+  <link fromOperation="Somatic Sniper" fromProperty="output_file" toOperation="Tier Variants" toProperty="variant_file" />
+  
+  <link fromOperation="Sniper Adaptor" fromProperty="output_file" toOperation="Annotate Transcript Variants" toProperty="variant_file" />
+
+  <link fromOperation="Annotate Transcript Variants" fromProperty="output_file" toOperation="Tier Variants" toProperty="transcript_annotation_file" />
+
+  <link fromOperation="Tier Variants" fromProperty="output_file" toOperation="output connector" toProperty="tier_file" />
+
+  <operation name="Somatic Sniper">
+    <operationtype commandClass="Genome::Model::Tools::Somatic::Sniper" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operation name="Sniper Adaptor">
+    <operationtype commandClass="Genome::Model::Tools::Annotate::Adaptor::Sniper" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operation name="Annotate Transcript Variants">
+    <operationtype commandClass="Genome::Model::Tools::Annotate::TranscriptVariants" typeClass="Workflow::OperationType::Command" />
+  </operation>
+  <operation name="Tier Variants">
+    <operationtype commandClass="Genome::Model::Tools::Somatic::TierVariants" typeClass="Workflow::OperationType::Command" />
+  </operation>
+
+  <operationtype typeClass="Workflow::OperationType::Model">
+    <inputproperty>normal_file</inputproperty>
+    <inputproperty>tumor_file</inputproperty>
+    <inputproperty>sniper_output</inputproperty>
+    <inputproperty>adaptor_output</inputproperty>
+    <inputproperty>annotate_output</inputproperty>
+    <inputproperty>tier_output</inputproperty>
+    <inputproperty>ucsc_file</inputproperty>
+    <outputproperty>tier_file</outputproperty>
+  </operationtype>
+
+</workflow>
