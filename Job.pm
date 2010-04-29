832a3e7c2bf45453cba3237948f193d75556a5ec
eclark <eclark@genome.wustl.edu>
2010-04-29 19:17:25 +0000

    Minimal set of classes to represent lsf jobs.  You can get an object by calling:
    my $j = Workflow::Job::Lsf->get(12345);
    
    or
    
    my $j = Workflow::Job->get(type => 'lsf', job_id => 12345);
    
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@58109 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Job.pm
@@ -0,0 +1,81 @@
+package Workflow::Job;
+
+use strict;
+use Workflow;
+use Workflow::Job::Event;
+
+class Workflow::Job {
+    is          => 'UR::Value',   ## necessary for UR to call _load during get()
+    is_abstract => 1,
+    sub_classification_method_name => '_resolve_subclass_name',
+    id_by                          => [
+        job_id => {
+            is  => 'Number',
+            doc => 'id that is locally unique to the job dispatcher'
+        }
+    ],
+    has => [
+        type => {
+            is  => 'String',
+            doc => 'used to resolve the subclass'
+        },
+    ],
+    has_many => [
+        events => {
+            is         => 'Workflow::Job::Event',
+            reverse_as => 'job'
+        }
+    ]
+};
+
+sub _load {
+    my $class = shift;
+    my $rule  = shift;
+
+    $DB::single = 1;
+
+    my @obj = $UR::Context::current->get_objects_for_class_and_rule($class, $rule);
+
+    unless (@obj) {
+        my $subclass = $class->_resolve_subclass_name($rule->params_list);
+        @obj = $subclass->_create_object($rule);
+        for (@obj) { $_->__signal_change__("load"); }
+    }
+
+    return @obj;
+}
+
+sub type {
+    my $self = shift;
+    if (@_) {
+        die 'cannot change type after creation';
+    }
+    if ( my $type = $self->__type ) {
+        return $type;
+    } else {
+        return $self->__type( lc( ( split( '::', ref($self) ) )[-1] ) );
+    }
+}
+
+sub _resolve_subclass_name {
+    my $class = shift;
+    my $subclass_name;
+
+    $DB::single = 1;
+
+    if ( $class ne __PACKAGE__ ) {
+        $subclass_name = $class;
+    } elsif ( my $type =
+        $class->get_rule_for_params(@_)
+        ->specified_value_for_property_name('type') )
+    {
+        $subclass_name = __PACKAGE__ . '::' . ucfirst( $type );
+    } else {
+        die 'dont know how to subclass';
+    }
+
+    $subclass_name->class if $subclass_name;
+    return $subclass_name;
+}
+
+1;
