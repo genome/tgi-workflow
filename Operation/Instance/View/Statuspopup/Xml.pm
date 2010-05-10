469686bf5acc69db309c971f8c6a7d8ef90ae06c
eclark <eclark@genome.wustl.edu>
2010-05-10 21:07:20 +0000

    reorganized some css for the status perspective and added a jquery ajax "loading" div use Ajax to load workflow status information into the build status view.
    git-svn-id: svn+ssh://svn/srv/svn/gscpan/perl_modules/trunk/Workflow@58529 127c0c0e-971a-0410-ad48-868c1a942d8d

---
+++ Operation/Instance/View/Statuspopup/Xml.pm
@@ -0,0 +1,7 @@
+package Workflow::Operation::Instance::View::Statuspopup::Xml;
+
+class Workflow::Operation::Instance::View::Statuspopup::Xml {
+    is  => 'Workflow::Operation::Instance::View::Status::Xml',
+};
+
+1;
