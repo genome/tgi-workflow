c51a0a68c3982ccb3e91a678b2e4de0dcf8e2756
eclark <eclark@genome.wustl.edu>
2010-08-05 13:52:12 -0500

    IPC::Open3 version of cleanup, so it can check stderr.

--- lib/Workflow/Command/Ns/Internal/Cleanup.pm
+++ lib/Workflow/Command/Ns/Internal/Cleanup.pm
@@ -4,7 +4,8 @@ use strict;
 use warnings;
 
 use Workflow ();
-use IPC::Run qw(start);
+use IPC::Open3 qw(open3);
+use IO::Select;
 
 class Workflow::Command::Ns::Internal::Cleanup {
     is => ['Workflow::Command'],
@@ -16,31 +17,70 @@ sub execute {
     my $self = shift;
 
     my @jobid = ();
-
     my @cmd = ( qw(bjobs -g), $self->job_group, qw(-u all -p) );
 
-    open( my $fh, '-|', join( ' ', @cmd ) )
-      or die "cant open bjobs: $!";
+    my $s = IO::Select->new();
 
-    my $head = <$fh> or die 'no header';
+    my $wtr = \*WTR;
+    my $err = \*ERR;
 
-    if ($head =~ /No job found in job group/) {
-        return 1;
-    }
+    my $pid = open3( '<&STDIN', $wtr, $err, join( ' ', @cmd ) );
 
-    while ( my $jobline = <$fh> ) {
-        chomp $jobline;
-        my $pendreason = <$fh>;
+    $s->add( $wtr, $err );
 
-        if ( $pendreason =~ /Dependency condition invalid or never satisfied;/ )
-        {
-            my $jobid = ( $jobline =~ /^(\d+)/ )[0];
+    my $ebuf = '';
+    my $obuf = '';
 
-            push @jobid, $jobid;
+  O: while ( my @ready = $s->can_read() ) {
+        foreach my $fh (@ready) {
+            if ( $fh == $err ) {
+                my $len = sysread $err, $ebuf, 4096, length($ebuf);
+
+                if ( $len == 0 ) {
+                    warn 'eof e';
+                    $s->remove($err);
+                } elsif ( $len == -1 ) {
+                    die "read err: $!";
+                } elsif ( $len < 4096 ) {
+                    my $line = substr( $ebuf, 0, index( $ebuf, "\n" ) + 1, '' );
+
+                    if ( $line =~ /No job found in job group/ ) {
+                        return 1;
+                    }
+                }
+
+            } elsif ( $fh == $wtr ) {
+                my $len = sysread $wtr, $obuf, 4096, length($obuf);
+
+                if ( $len == 0 ) {
+                    warn 'eof o';
+                    $s->remove($wtr);
+                } elsif ( $len == -1 ) {
+                    die "read out: $!";
+                } elsif ( $len < 4096 ) {
+                    if ( my $a = index( $obuf, "\n" ) ) {
+                        if ( my $b = index( $obuf, "\n", $a + 1 ) ) {
+                            my $pendline = substr( $obuf,     0, $b + 1, '' );
+                            my $jobline  = substr( $pendline, 0, $a + 1, '' );
+
+                            if ( $pendline =~
+/Dependency condition invalid or never satisfied;/
+                              )
+                            {
+                                my $jobid = ( $jobline =~ /^(\d+)/ )[0];
+                                push @jobid, $jobid;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    close $fh;
+    close $wtr;
+    close $err;
+
+    waitpid $pid, 0;
 
     my $ok = 1;
     foreach my $jobid (@jobid) {
