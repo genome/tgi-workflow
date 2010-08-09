5306e62215e9bd7b4baffa1c358a51e1d56dc957
eclark <eclark@genome.wustl.edu>
2010-08-09 10:39:46 -0500

    deferred restarting behavior

--- lib/Workflow/Command/Ns/Internal/End.pm
+++ lib/Workflow/Command/Ns/Internal/End.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Workflow ();
+use Workflow::Command::Ns::Start ();
 
 class Workflow::Command::Ns::Internal::End {
     is  => ['Workflow::Command'],
@@ -11,6 +12,10 @@ class Workflow::Command::Ns::Internal::End {
         instance_id => {
             shell_args_position => 1,
             doc                 => 'Instance id that had an operation exit'
+        },
+        done_instance_id => {
+            shell_args_position => 2,
+            is_optional         => 1
         }
     ]
 };
@@ -18,14 +23,75 @@ class Workflow::Command::Ns::Internal::End {
 sub execute {
     my $self = shift;
 
-    print "something exited\n";
+    my $jobid    = $ENV{LSB_JOBID};
+    my $jobgroup = $ENV{LSB_JOBGROUP};
+
+    die unless ($jobgroup && $jobgroup ne '');
+
+    unless ($jobid) {
+        $self->error_message("command must be run under lsf");
+        return;
+    }
+    if ( !-e "/gscuser/eclark/stop" ) {
+
+        print "resched $jobid\n";
+
+        my ( $sec, $min, $hour, $day, $mon, $year ) =
+          localtime( time + 15 * 60 );
+        $year += 1900;    ## i hate you perl
+        $mon++;
+
+        my $cmd = "bsub -q short -b $year:$mon:$day:$hour:$min -g $jobgroup workflow ns internal end " . $self->instance_id;
+        if (my $did = $self->done_instance_id) {
+            $cmd .= ' ' . $did;
+        }
+
+        $self->status_message('lsf$ ' . $cmd);
+        my $bsub_output = `$cmd`;
+
+        $self->status_message($bsub_output);
+
+        if ($?) {
+            $self->error_message("couldn't bsub myself");
+            return -88;
+        }
+
+        my $jobid;
+        if ( $bsub_output =~ /^Job <(\d+)> is submitted to queue <(\w+)>\./m ) {
+            $jobid = $1;
+        } else {
+            $self->error_message("cant parse job id from bsub output");
+            return -92;
+        }
+
+        if (my $done_instance_id = $self->done_instance_id) {
+            my $done_instance = Workflow::Operation::Instance->get(
+                $done_instance_id
+            );
+
+            my $starter = Workflow::Command::Ns::Start->create(
+                job_group => $jobgroup
+            );
+
+            my $r = $starter->bsub_operation( undef, $done_instance, $done_instance, $jobid );
+
+            $starter->delete;
+
+        }
+
+        exit -80;
+    } else {
+        print "noresched $jobid\n";
+    }
+
+    print "done\n";
 
     # load workflow
     # find running sub workflows with crashed events
     # if no other event running, set workflow crashed
     # walk upward to parent
 
-    # if things claim to be running, compare bjobs -g 
+    # if things claim to be running, compare bjobs -g
     # to database list
     # when bad status found
     #  lock row, change status
@@ -37,7 +103,7 @@ sub execute {
     # that will never have deps satisfied
     # kill done handler
 
-    # exit successfully so user-defined handler runs 
+    # exit successfully so user-defined handler runs
 
     1;
 }
