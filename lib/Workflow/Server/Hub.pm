d8ca4848ddf6fabd3be9ba0f6d0ef8e8c315b34e
eclark <eclark@genome.wustl.edu>
2010-08-27 17:23:49 -0500

    Per conversation with Rich, look for /usr/local/lsfmaster first.  This code will be removed soon and we will use a postexec hook or wrapper that caalls getrusage

--- lib/Workflow/Server/Hub.pm
+++ lib/Workflow/Server/Hub.pm
@@ -141,8 +141,15 @@ sub setup {
                 $kernel->alias_set("lsftail");
                 $kernel->call('IKC','publish','lsftail',[qw(add_watcher delete_watcher quit)]);
 
+                my $filename = "/usr/local/lsf/work/gsccluster1/logdir/lsb.acct";
+                my $newfilename = "/usr/local/lsfmaster/work/gsccluster1/logdir/lsb.acct";
+
+                if (-e $newfilename && -r _) {
+                    $filename = $newfilename;
+                }
+
                 $heap->{monitor} = POE::Wheel::FollowTail->new(
-                    Filename => "/usr/local/lsf/work/gsccluster1/logdir/lsb.acct",
+                    Filename => $filename,
                     InputEvent => 'handle_input',
                     ResetEvent => 'handle_reset',
                     ErrorEvent => 'handle_error'
