34a6fb92b332150e4567ec06eba845dc47d5913a
eclark <eclark@genome.wustl.edu>
2010-09-28 16:12:40 -0500

    Fixes all but 40_save_load.t

--- lib/Workflow/DataSource/Local.pm
+++ lib/Workflow/DataSource/Local.pm
@@ -3,16 +3,35 @@ package Workflow::DataSource::Local;
 use strict;
 use warnings;
 
-use Workflow;
+use Workflow ();
+use File::Temp qw(tempdir);
 
 class Workflow::DataSource::Local {
     is => [ 'UR::DataSource::SQLite', 'UR::Singleton' ],
 };
 
-sub server { 
+our $sqlite_db;
+
+sub server {
+    return $sqlite_db if defined $sqlite_db;
+
+    my $self = shift;
     my $file = __FILE__;
+
     $file =~ s/.pm$/.sqlite3/;
+
+    unless (-e $file && -w _) {
+        my $dir = tempdir( CLEANUP => 1);
+        $self->make_sqlite3_db("$file-schema",$file = "$dir/local.sqlite3");
+    }
+    $sqlite_db = $file;
     return $file;
+}
+
+sub make_sqlite3_db {
+    my ($self, $schema_file, $file) = @_;
+
+    system("sqlite3 $file <$schema_file");
 }
 
 1;
