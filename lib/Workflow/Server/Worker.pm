1693918ec5208b24ce8b447adf9533b7b5a0e0b5
Matt Callaway <mcallawa@genome.wustl.edu>
2010-11-18 14:54:11 -0600

    Add the details of running collectl to gather metrics.

--- lib/Workflow/Server/Worker.pm
+++ lib/Workflow/Server/Worker.pm
@@ -4,9 +4,12 @@ package Workflow::Server::Worker;
 use strict;
 
 use AnyEvent::Impl::POE;
+use AnyEvent::Util;
 use AnyEvent;
 use POE;
 
+use File::Copy;
+use File::Basename;
 use POE::Component::IKC::Client;
 use Workflow::Server::Hub;
 use Error qw(:try);
@@ -62,26 +65,33 @@ sub __build {
             execute => sub {
                 my ($kernel, $heap, $arg) = @_[KERNEL, HEAP, ARG0];
                 my ($instance, $type, $input, $sc_flag, $out_log, $err_log) = @$arg;
-                
+
                 $kernel->alarm_remove_all;
 
                 $ENV{'WORKFLOW_PARENT_EXECUTION'} = $instance->{current_execution_id};
 
                 my $collectl_cv;
                 my $collectl_pid;
-                if ($ENV{'WORKFLOW_COLLECTL'} && -e $err_log) {
-                    ## munge error log name to be the collectl name
-                    
+                my $collectl_cmd = "/usr/bin/collectl";
+                my $collectl_args = "--all --export lexpr -f /tmp -on";
+                my $collectl_output;
+                if ($ENV{'WF_PROFILER'} && -e $err_log) {
+                    if (! -x $collectl_cmd) {
+                        warn "WF_PROFILER is enabled, but $collectl_cmd is not present and executable\n";
+                    }
+                    # Both WF_PROFILER and error logging must be set in order to be
+                    # sure that we have a path to put our profiling stats in.
+                    $collectl_output = $err_log;
+                    $collectl_output =~ s/.err/.collectl.out/;
                     $collectl_cv = AnyEvent::Util::run_cmd(
-                        "###FIXME collectl cmd",
+                        "$collectl_cmd $collectl_args",
                         close_all => 1,
                         '$$' => \$collectl_pid,
-                        on_prepare => sub {
-                            # in child
-                        }
+                        on_prepare => sub { delete $ENV{PERL5LIB} }
                     );
 
                     my $cv = AnyEvent->condvar;
+                    # We use a timer to give collectl time to start up.
                     my $w = AnyEvent->timer(
                         after => 2,
                         cb => $cv
@@ -120,6 +130,9 @@ sub __build {
 
                 if ($collectl_cv) {
                     ## shut down collectl
+                    kill 15, $collectl_pid;
+                    $collectl_cv->recv;
+                    move "/tmp/L", $collectl_output or die "Failed to move collectl output file /tmp/L to $collectl_output: $!";
                 }
 
                 $kernel->post('IKC','post','poe://Hub/dispatch/end_work',[$job_id, $kernel->ID, $instance->id, $status, $output, $error_string]);
