6da16baadbc16227e94396efa1e8855a931b750e
Matt Callaway <mcallawa@genome.wustl.edu>
2010-12-16 16:26:03 -0600

    Make note that /usr/bin/time isn't going to work with this model.

--- lib/Workflow/Metrics/Usrbintime.pm
+++ lib/Workflow/Metrics/Usrbintime.pm
@@ -12,6 +12,7 @@ sub run {
 
   die "/usr/bin/time is not present and executable" if (! -x "/usr/bin/time");
 
+  # FIXME: This isn't going to work.
   my $timecmd = '/usr/bin/time -f "command: %C\ntime.user: %U\ntime.sys: %S\ntime.wall: %e\ncpu: %P\nmem.avg.shared.text: %X\nmem.avg.unshared.data: %D\nmem.avg.stack: %p\nmem.avg.total: %K\nmem.max.resident: %M\nmem.avg.resident: %t\nmem.maj.fault: %F\nmem.min.fault: %R\ncontext.vol: %w\ncontext.inv: %c\nswaps: %W\nfs.inputs: %I\nfs.outputs: %O\nsocket.sent: %s\nsocket.rcvd: %r\nsignals.delivered: %k\npage.size: %Z\nexit.status: %x\n"' . " -o $self->{metrics}";
 
   return AnyEvent::Util::run_cmd(
