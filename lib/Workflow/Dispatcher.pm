cb90ea20fe74570c171b75e4fa8a35c28183c445
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-04-01 16:33:42 -0500

    Initial Dispatcher work and tests. Tests failing

---
+++ lib/Workflow/Dispatcher.pm
@@ -0,0 +1,31 @@
+package Workflow::Dispatcher;
+
+use strict;
+use warnings;
+
+BEGIN {
+    my @dispatchers = [];
+    sub dispatchers { return @dispatchers; }
+    sub dispatcher_add{ my $dis; push(@dispatchers, $dis); }
+}
+
+class Workflow::Dispatcher {
+    has => [
+        cluster => { is => 'Text' },
+    ]
+};
+
+sub execute {
+    die('Must be implemented in a subclass');
+}
+
+sub get_or_create {
+    my $class = shift;
+    my $engine = shift;
+    my $cluster = shift;
+    print $engine;
+    return "Workflow::Dispatcher::$engine"->create(
+        engine => $engine,
+        cluster => $cluster
+    );
+}
--- lib/Workflow/Resource.pm
+++ lib/Workflow/Resource.pm
@@ -1,19 +1,12 @@
 package Workflow::Resource;
 
+# -R 'select[ncpus >= $ && mem >= $, gtmp >= $] span[hosts=1] rusage[mem=$, gtmp=$]' -M $kb -n $cpus -q $queue
+
 class Workflow::Resource {
     has => [
-        -R 'select[ncpus >= $ && mem >= $, gtmp >= $] span[hosts=1] rusage[mem=$, gtmp=$]' -M $kb -n $cpus -q $queue
         mem_limit => { is => 'Number', doc => 'Memory limit of executing job. (MB)' },
-        min_proc => { is => 'Number', doc => 'Minimum number of processors.' is_optional => 1 },
-        max_proc => { is => 'Number', doc => 'Maximum number of processors.', default_value => 0 },
+        min_proc => { is => 'Number', doc => 'Minimum number of processors.', is_optional => 1 },
+        max_proc => { is => 'Number', doc => 'Maximum number of processors.', is_optional => 1 },
         time_limit => { is => 'Number', doc => 'Maximum CPU time for job.', is_optional => 1 },
-
-        Sun Grid Engine:
-
-        h_cpu -> per-job maximum memory limit in bytes
-        s_cpu -> per-process cpu time limit in seconds
-        h_data -> per-job maximum memory limit in bytes
-
-
     ]
 };
