be1b32a076323660a2312ff564c460fe948bc3e7
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-04-07 15:11:05 -0500

    add tmp_space option to Workflow::Resource

--- lib/Workflow/Resource.pm
+++ lib/Workflow/Resource.pm
@@ -5,7 +5,8 @@ package Workflow::Resource;
 class Workflow::Resource {
     has => [
         mem_limit => { is => 'Number', doc => 'Memory limit of executing job. (MB)' },
-        min_proc => { is => 'Number', doc => 'Minimum number of processors.', is_optional => 1 },
+        tmp_space => { is => 'Number', doc => 'Temp space needed. (GB)', default_value => 1 },
+        min_proc => { is => 'Number', doc => 'Minimum number of processors.', is_optional => 1, default_value => 1 },
         max_proc => { is => 'Number', doc => 'Maximum number of processors.', is_optional => 1 },
         time_limit => { is => 'Number', doc => 'Maximum CPU time for job.', is_optional => 1 },
     ]
