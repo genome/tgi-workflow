55cd5e1b6cea269debfe200c404e7279a14046eb
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-04-08 12:33:31 -0500

    add mem_request attribute to Resource requests

--- lib/Workflow/Resource.pm
+++ lib/Workflow/Resource.pm
@@ -7,5 +7,6 @@ class Workflow::Resource {
         min_proc => { is => 'Number', doc => 'Minimum number of processors.', is_optional => 1, default_value => 1 },
         max_proc => { is => 'Number', doc => 'Maximum number of processors.', is_optional => 1 },
         time_limit => { is => 'Number', doc => 'Maximum CPU time for job.', is_optional => 1 },
+        mem_request => { is => 'Number', doc => 'Memory allocation request. (MB)', is_optional => 1 }
     ]
 };
