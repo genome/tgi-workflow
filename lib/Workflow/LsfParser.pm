77d60a3aa5c6ba64b176850faab9be3b1851e560
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-04-15 13:29:20 -0500

    continued work on lsfResource parsing

--- lib/Workflow/LsfParser.pm
+++ lib/Workflow/LsfParser.pm
@@ -9,3 +9,38 @@ class Workflow::LsfParser {
         queue => { is => 'String' },
     ],
 };
+
+sub get_resource_from_lsf_resource {
+    my $lsf_resource = shift;
+    my $resource = Workflow::Resource->create();
+    # parse mem limit -M ###kb
+    my ($mem_limit) = ($lsf_resource =~ /-M\s(\d+)/);
+    if (defined $mem_limit) {
+        $mem_limit = $mem_limit / 1024;
+        $resource->mem_limit($mem_limit);
+    }
+    # parse cpucs -n cpus
+    my ($min_proc) = ($lsf_resource =~ /-n\s(\d+)/);
+    if (defined $min_proc) {
+        $resource->min_proc($min_proc);
+    }
+    # handle rusage section
+    my ($rusage) = ($lsf_resource =~ /rusage\[([^\]]*)/);
+    # parse mem request rusage[mem=###mb, ...]
+    my ($mem_request) = ($rusage =~ /mem=(\d+)/); 
+    if (defined $mem_request) {
+        $resource->mem_request($mem_request);
+    }
+    # parse tmp request rusage[gtmp=###gb ...]
+    my ($gtmp) = ($rusage =~ /gtmp=(\d+)/);
+    if (defined $gtmp) {
+        $resource->tmp_space($gtmp);
+    }
+    # if gtmp didnt do it, there should be info in
+    # tmp. gtmp is genome center specific and avoids
+    # a problem with lsfs tmp disk allocation sys
+    my ($tmp) = ($rusage =~ /tmp=(\d+)/);
+    if (defined $tmp) {
+        $resource->tmp_space($tmp);
+    }
+}
--- lib/Workflow/Resource.pm
+++ lib/Workflow/Resource.pm
@@ -2,7 +2,7 @@ package Workflow::Resource;
 
 class Workflow::Resource {
     has => [
-        mem_limit => { is => 'Number', doc => 'Memory limit of executing job. (MB)' },
+        mem_limit => { is => 'Number', doc => 'Memory limit of executing job. (MB)', is_optional => 1 },
         tmp_space => { is => 'Number', doc => 'Temp space needed. (GB)', default_value => 1 },
         min_proc => { is => 'Number', doc => 'Minimum number of processors.', is_optional => 1, default_value => 1 },
         max_proc => { is => 'Number', doc => 'Maximum number of processors.', is_optional => 1 },
