dcdbe4bac4d83fa4fad1003e1d95b4240af92837
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-04-21 17:11:00 -0500

    Additional body to Sge Dispatcher class

--- lib/Workflow/Dispatcher/Sge.pm
+++ lib/Workflow/Dispatcher/Sge.pm
@@ -10,7 +10,19 @@ class Workflow::Dispatcher::Sge {
 sub execute {
     my $self = shift;
     my $job = shift;
+    my $cmd = $self->get_command($job);
+    my $sge_output = `$cmd`;
+    my ($job_id) = ($sge_output = /Your job (\d+)/);
+    return $job_id;
 }
 
 sub get_command {
+    my $self = shift;
+    my $job = shift;
+    my $cmd = "qsub ";
+    if (defined $job->resource->mem_free) {
+        $cmd .= "-l mem_free=" . $job->resource->mem_free . "M ";
+    }
+    # set up resources
+    return $cmd;
 }
