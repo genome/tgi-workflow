c1e493d2c6eda47d1f29a38fadf521a9fb48fee4
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-05-13 14:18:32 -0500

    removed old comment about deprecated UR

--- lib/Workflow.pm
+++ lib/Workflow.pm
@@ -6,7 +6,6 @@ use strict;
 use UR;
 use Workflow::Time; # a copy of the old UR::Time
 
-# this keeps around old parts of the UR::Object API we removed in the 0.01 release
 use Carp qw{carp};
 
 class Workflow {
