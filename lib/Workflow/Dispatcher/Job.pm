edf857f326820f2f016fdc95ce8f3dacbb805d98
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-05-13 10:48:15 -0500

    added fields to conform to Workflow/Hub.pm usage

--- lib/Workflow/Dispatcher/Job.pm
+++ lib/Workflow/Dispatcher/Job.pm
@@ -4,6 +4,10 @@ class Workflow::Dispatcher::Job {
     has => [
         resource => { is => 'Workflow::Resource' },
         command => { is => 'Text' },
+        # group is e.g. /workflow-worker2 in workflow
+        group => { is => 'Text', is_optional => 1 },
+        name => { is => 'Text', is_optional => 1 },
+        project => { is => 'Text', is_optional => 1 },
         queue => { is => 'Text', is_optional => 1 },
         stdout => { is => 'Text', is_optional => 1 },
         stderr => { is => 'Text', is_optional => 1 }
