93229abf7459714a4e2a0e61f42ac6fe9687c7e4
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-05-20 13:56:39 -0500

    added xml serialization to Resource.pm

--- lib/Workflow/Resource.pm
+++ lib/Workflow/Resource.pm
@@ -16,3 +16,37 @@ class Workflow::Resource {
         mem_request => { is => 'Number', doc => 'Memory allocation request. (MB)', is_optional => 1 }
     ]
 };
+
+sub as_xml_simple_structure {
+    my $self = shift;
+
+    my $struct = {};
+
+    $struct->{memLimit} = $self->mem_limit if (defined $self->mem_limit);
+    $struct->{tmpSpace} = $self->tmp_space if (defined $self->tmp_space);
+    $struct->{maxTmp} = $self->max_tmp if (defined $self->max_tmp);
+    $struct->{useGtmp} = $self->use_gtmp if (defined $self->use_gtmp);
+    $struct->{minProc} = $self->min_proc if (defined $self->min_proc);
+    $struct->{maxProc} = $self->max_proc if (defined $self->max_proc);
+    $struct->{timeLimit} = $self->time_limit if (defined $self->time_limit);
+    $struct->{memRequest} = $self->mem_request if (defined $self->mem_request);
+
+    return $struct;
+}
+
+sub create_from_xml_simple_structure {
+    my ($cls, $struct) = @_;
+
+    my $self = $cls->create();
+
+    $self->mem_limit(delete $struct->{memLimit}) if (exists $struct->{memLimit});
+    $self->tmp_space(delete $struct->{tmpSpace}) if (exists $struct->{tmpSpace});
+    $self->max_tmp(delete $struct->{maxTmp}) if (exists $struct->{maxTmp});
+    $self->use_gtmp(delete $struct->{useGtmp}) if (exists $struct->{useGtmp});
+    $self->min_proc(delete $struct->{minProc}) if (exists $struct->{minProc});
+    $self->max_proc(delete $struct->{maxProc}) if (exists $struct->{maxProc});
+    $self->time_limit(delete $struct->{timeLimit}) if (exists $struct->{timeLimit});
+    $self->mem_request(delete $struct->{memRequest}) if (exists $struct->{memRequest});
+
+    return $self;
+}
