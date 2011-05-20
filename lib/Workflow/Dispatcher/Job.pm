c15e966d941b5227169415dedf05cfea71f663ce
Philip Kimmey <pkimmey@genome.wustl.edu>
2011-05-20 15:21:43 -0500

    added xml serialization for jobs

--- lib/Workflow/Dispatcher/Job.pm
+++ lib/Workflow/Dispatcher/Job.pm
@@ -12,4 +12,33 @@ class Workflow::Dispatcher::Job {
         stdout => { is => 'Text', is_optional => 1 },
         stderr => { is => 'Text', is_optional => 1 }
     ]
+};
+
+sub as_xml_simple_structure {
+    my $self = shift;
+
+    my $struct = {};
+    
+    $struct->{resource} = $self->resource->as_xml_simple_structure;
+
+    foreach my $key (qw/resource command group name project queue stdout stderr/) {
+        $struct->{$key} = $self->$key if (defined $self->$key);
+    }
+    return $struct;
+}
+
+sub create_from_xml_simple_structure {
+    my ($cls, $struct) = @_;
+
+    my $resource = Workflow::Resource->create_from_xml_simple_structure($struct->{resource});
+    my $self = $cls->create(
+        resource => $resource,
+        command => $struct->{command}
+    );
+
+    foreach my $key (qw/group name project queue stdout stderr/) {
+        $self->$key(delete $struct->{$key}) if (exists $struct->{$key});
+    }
+
+    return $self;
 }
