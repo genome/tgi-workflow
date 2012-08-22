799eb24de3b281ee769786b27aad4094d6ee3f9a
Thomas Mooney <tmooney@genome.wustl.edu>
2012-08-22 14:43:37 -0500

    Re-use the template for creating links.

--- lib/Workflow/Link.pm
+++ lib/Workflow/Link.pm
@@ -17,6 +17,13 @@ class Workflow::Link {
     ]
 };
 
+
+my @_properties_for_add_link = ('id','workflow_model_id','left_workflow_operation_id','left_property',
+                                'right_workflow_operation_id','right_property');
+my @_properties_in_template_order;
+my $_link_template;
+my $_workflow_id_position;
+my $_link_id_position;
 sub create_from_xml_simple_structure {
     my $class = shift;
     my $struct = shift;
@@ -24,7 +31,7 @@ sub create_from_xml_simple_structure {
 
     my %ops_by_name;
     if(exists $params{operations_by_name}) {
-        %ops_by_name = %{delete $params{operations_by_name}};
+        %ops_by_name = %{$params{operations_by_name}};
     } else {
         %ops_by_name = map {
             $_->name => $_
@@ -39,13 +46,46 @@ sub create_from_xml_simple_structure {
         Carp::confess('To operation not found: ' . $struct->{toOperation});
     }
 
-    my $self = $class->create(
-        left_operation => $ops_by_name{$struct->{fromOperation}},
-        left_property => $struct->{fromProperty},
-        right_operation => $ops_by_name{$struct->{toOperation}},
-        right_property => $struct->{toProperty},
-        %params
-    );
+    my $self;
+    unless(exists $params{breakable}) { #speed up the case where "breakable is not specified"
+        unless ($_link_template) {
+            my $tmpl = UR::BoolExpr::Template->resolve('Workflow::Link', @_properties_for_add_link);
+            unless ($tmpl) {
+                Carp::croak('Cannot resolve BoolExpr template for adding Workflow::Link objects');
+            }
+            $tmpl = $tmpl->get_normalized_template_equivalent();
+            for my $name ( @_properties_for_add_link ) {
+                my $pos = $tmpl->value_position_for_property_name($name);
+                if ($name eq 'workflow_model_id' ) {
+                    $_workflow_id_position = $pos;
+                } elsif ($name eq 'id') {
+                    $_link_id_position = $pos;
+                }
+                $_properties_in_template_order[$pos] = $name;
+            }
+            $_link_template = $tmpl;
+        }
+
+        $params{left_workflow_operation_id} = $ops_by_name{$struct->{fromOperation}}->id;
+        $params{left_property} = $struct->{fromProperty};
+        $params{right_workflow_operation_id} = $ops_by_name{$struct->{toOperation}}->id;
+        $params{right_property} = $struct->{toProperty};
+
+        my @values = @params{@_properties_in_template_order};
+        $values[$_workflow_id_position] = $params{workflow_model}->id;
+        $values[$_link_id_position] = UR::Object::Type->autogenerate_new_object_id_urinternal();
+        my $rule = $_link_template->get_rule_for_values(@values);
+        $self = UR::Context->create_entity('Workflow::Link', $rule);
+
+    } else {
+        $self = $class->create(
+            left_operation => $ops_by_name{$struct->{fromOperation}},
+            left_property => $struct->{fromProperty},
+            right_operation => $ops_by_name{$struct->{toOperation}},
+            right_property => $struct->{toProperty},
+            %params
+        );
+    }
 
     return $self;
 }
