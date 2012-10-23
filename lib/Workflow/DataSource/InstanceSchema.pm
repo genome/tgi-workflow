20a2763b624fe24f669a4a08a4f6b5b58128e7b6
Scott Smith <ssmith@genome.wustl.edu>
2012-10-23 17:03:19 -0500

    cosmetic cleanup of pg-only datasource

--- lib/Workflow/DataSource/InstanceSchema.pm
+++ lib/Workflow/DataSource/InstanceSchema.pm
@@ -5,13 +5,13 @@ use Carp;
 
 class Workflow::DataSource::InstanceSchema {
     is => ['UR::DataSource::Pg'],
-    type_name => 'workflow datasource instanceschema',
-	has_constant => [
-	        server => { default_value => 'dbname=genome;host=gms-postgres' },
-	        login => { default_value => 'genome' },
-	        auth => { default_value => 'TGIlab' },
-	        owner => { default_value => 'workflow' },
-	    ],
+    has_constant => [
+        server => { default_value => 'dbname=genome;host=gms-postgres' },
+        login => { default_value => 'genome' },
+        auth => { default_value => 'TGIlab' },
+        owner => { default_value => 'workflow' },
+	  ],
 };
 
 1;
+
