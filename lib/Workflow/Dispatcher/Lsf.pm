d632a5bf031d90084286e9530d01c5c3dcc2cf6b
Scott Smith <ssmith@genome.wustl.edu>
2013-07-28 14:04:18 -0500

    Revert "restored rusage to use comma-delimited elements intead of multiple rusage elements"
    This reverts commit 810ef161c1c2a0a5722a17eed1cd6f2b24e77ea8.

--- lib/Workflow/Dispatcher/Lsf.pm
+++ lib/Workflow/Dispatcher/Lsf.pm
@@ -66,12 +66,18 @@ sub get_command {
         }
     }
     
-    my $rusage = join(", ", @rusages);
-    if ($rusage ne "") {
-        $cmd .= sprintf(" rusage[%s]' ", $rusage);
-    } else {
-        $cmd .= "' ";
+    #my $rusage = join(", ", @rusages);
+    #if ($rusage ne "") {
+    #    $cmd .= sprintf(" rusage[%s]' ", $rusage);
+    #} else {
+    #    $cmd .= "' ";
+    #}
+
+    # This is longer than the above but works with regular LSF and also OpenLava.
+    if (@rusages) {
+        $cmd .= " " . join(" ", map { "rusage[$_]" } @rusages);
     }
+    $cmd .= "' ";
 
     # add memory & number of cores requirements
     if (defined $job->resource->mem_limit) {
