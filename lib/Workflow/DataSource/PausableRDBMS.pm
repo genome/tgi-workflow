1c508f8adaf24a0a292942a87eaefc18319d6b55
Anthony Brummett <abrummet@genome.wustl.edu>
2014-02-28 14:41:57 -0600

    remove tee that syncs to postgres
    Change-Id: I1bc544b2e87cf210c35b0c5a9257d967964a406c

---
+++ lib/Workflow/DataSource/PausableRDBMS.pm
@@ -0,0 +1,62 @@
+package Workflow::DataSource::PausableRDBMS;
+
+use Workflow;
+
+class Workflow::DataSource::PausableRDBMS {
+    doc => 'Mixin class to implement pausing access to the database',
+};
+
+my $query_pause = _make_db_pause_function('query_pause_sentry_file_path');
+foreach my $signal ( qw( query precreate_handle sequence_nextval ) ){
+    __PACKAGE__->create_subscription(
+        method => $signal,
+        callback => $query_pause,
+    );
+}
+
+my $commit_pause = _make_db_pause_function('commit_pause_sentry_file_path');
+UR::Context->current->create_subscription(
+    method => 'precommit',
+    callback => sub {
+        __PACKAGE__->$commit_pause;
+    }
+);
+
+sub query_pause_sentry_file_path {
+    $ENV{GENOME_DB_QUERY_PAUSE};
+}
+
+sub commit_pause_sentry_file_path {
+    $ENV{GENOME_DB_PAUSE};
+}
+
+sub pause_sleep_length_seconds { 30 }
+
+sub _make_db_pause_function {
+    my $pause_check_method = shift;
+
+    return sub {
+        my $self = shift;
+
+        my $sentry = $self->$pause_check_method;
+        return 1 unless $sentry and -e $sentry;
+
+        print STDERR "Database querying has been paused; disconnecting db handles.  "
+                    . "Please wait until the query pause is released...\n";
+
+        my @data_sources = UR::Context->all_objects_loaded(__PACKAGE__);
+        for my $ds (@data_sources) {
+            $ds->disconnect_default_handle if $ds->has_default_handle;
+        }
+
+        while (1) {
+            sleep pause_sleep_length_seconds;
+            last unless $sentry and -e $sentry;
+        }
+
+        print STDERR "Database updating has been resumed, continuing query!\n";
+        return 1;
+    };
+}
+
+1;
